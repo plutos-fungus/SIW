42131429b8779b951a9433bae32fe4c16fd25bfa
Plutos-Fungus <rasmus1070@gmail.com>
2022-02-16 14:19:08 +0100

    Added a suggestion to a operation manager

---
+++ SIW/SIW/OperationManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIW
+{
+    public class OperationManager
+    {
+        // A suggestion to an comparison operation manager
+        public bool IsOrNotEqual(int int1, int int2)
+        {
+            if(int1 == int2)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+        public bool IsOrNotBigger(int int1, int int2)
+        {
+            if(int1 > int2)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+        public bool IsBiggerOrEqualTo(int int1, int int2)
+        {
+            if (int1 >= int2)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
