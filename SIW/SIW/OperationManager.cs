03e5a1288ff0e6ba3756843db21ba83120c745c8
Plutos-Fungus <rasmus1070@gmail.com>
2022-02-16 15:33:53 +0100

    Added a suggestion for checking if a value is a string or an int

--- SIW/SIW/OperationManager.cs
+++ SIW/SIW/OperationManager.cs
@@ -9,12 +9,51 @@ namespace SIW
     public class OperationManager
     {
         // A suggestion to an comparison operation manager
+        private string tempName;
+
+        // Check if something is a string or an int
+        public void IsType(string string1, string string2, string opperation, int int1, int int2)
+        {
+            if (int.TryParse(string1, out int1) && int.TryParse(string2, out int2))
+            {
+                tempName = "I";
+                ChooserInt(tempName, opperation, int1, int2);
+            }
+            else
+            {
+                tempName = "S";
+                ChooserString(tempName, opperation, string1, string2);
+            }
+        }
+
+        public void ChooserInt(string tempName, string opperation, int int1, int int2)
+        {
+            // If tempName is "I", then the type is an int
+            if (opperation == "==")
+            {
+                IsOrNotEqual(int1, int2);
+            }
+            else if (tempName == "I" && opperation == ">")
+            {
+                IsOrNotBigger(int1, int2);
+            }
+            else if (tempName == "I" && opperation == ">=")
+            {
+                IsBiggerOrEqualTo(int1, int2);
+            }
+        }
+    
+        public void ChooserString(string tempName, string opperation, string string1, string string2)
+        {
+            // Do string comparison opperation here
+        }
+
         public bool IsOrNotEqual(int int1, int int2)
         {
-            if(int1 == int2)
+            if (int1 == int2)
             {
                 return true;
-            } 
+            }
             else
             {
                 return false;
@@ -22,10 +61,10 @@ namespace SIW
         }
         public bool IsOrNotBigger(int int1, int int2)
         {
-            if(int1 > int2)
+            if (int1 > int2)
             {
                 return true;
-            } 
+            }
             else
             {
                 return false;
@@ -36,7 +75,7 @@ namespace SIW
             if (int1 >= int2)
             {
                 return true;
-            } 
+            }
             else
             {
                 return false;
